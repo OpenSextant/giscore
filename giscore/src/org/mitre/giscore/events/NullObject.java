0a5896f2684d80a3d5e00148fca409d510f0533d
Jason Mathews <mathews@mitre.org>
2009-03-09 13:09:40 +0000

    NullObject indicates an element was skipped but recoverable and end of file was not reached
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@61 5c99b798-f2dd-11dd-9a12-8f84353f4b73

---
+++ giscore/src/org/mitre/giscore/events/NullObject.java
@@ -0,0 +1,35 @@
+package org.mitre.giscore.events;
+
+import org.mitre.giscore.output.StreamVisitorBase;
+
+/**
+ * Create a NullObject to indicate an XML element was skipped
+ * but parse is recoverable and end of stream not reached yet.
+ * 
+ * @author Jason Mathews, MITRE Corp.
+ * Date: Mar 6, 2009 2:17:03 PM
+ */
+public class NullObject implements IGISObject {
+
+    private static final NullObject NULL_OBJECT = new NullObject();
+
+    private NullObject() {
+        // no instances other than static instance
+    }
+
+    /**
+     * Get NullObject instance
+     * @return NullObject
+     */
+    public static NullObject getInstance() {
+        return NULL_OBJECT;
+    }
+
+    /**
+     * Visit the object
+     *
+     * @param visitor the visitor to dispatch to, never <code>null</code>
+     */
+    public void accept(StreamVisitorBase visitor) {
+    }
+}
