48f25de93eac6589dd5cae989beb91e54d08da90
Douglas Rand <drand@mitre.org>
2009-04-07 18:42:45 +0000

    Added hints to the doc type w.r.t. zip entries and zip streams on output.
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@190 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/src/org/mitre/giscore/DocumentType.java
+++ giscore/src/org/mitre/giscore/DocumentType.java
@@ -18,6 +18,8 @@
  ***************************************************************************************/
 package org.mitre.giscore;
 
+import org.mitre.giscore.output.IGISOutputStream;
+
 /**
  * Values to identify the various formats to the framework. Extend this list
  * as new formats are added.
@@ -26,16 +28,40 @@ package org.mitre.giscore;
  */
 public enum DocumentType {
 	/* Google's KML Format */
-	KML,
+	KML(false,false),
 	/* Google's KMZ Format */
-	KMZ,
+	KMZ(true,true),
 	/* An ESRI format */
-	Shapefile,
+	Shapefile(true,false),
 	/* Access GDB output, an ESRI format */
-	PersonalGDB,
+	PersonalGDB(true,false),
 	/* File GDB output, an ESRI format */
-	FileGDB,
+	FileGDB(true,false),
 	/* An xml interchange format for ESRI Geodatabase data */
-	XmlGDB,
+	XmlGDB(false,false),
 	;
+	
+	private boolean requiresZipStream;
+	private boolean requiresZipEntry;
+	
+	private DocumentType(boolean reqZip, boolean reqZipEntry) {
+		requiresZipStream = reqZip;
+		requiresZipEntry = reqZipEntry;
+	}
+	
+	/**
+	 * @return <code>true</code> if this kind of type requires a zip output
+	 * stream for the {@link IGISOutputStream}. 
+	 */
+	public boolean requiresZipStream() {
+		return requiresZipStream;
+	}
+	
+	/**
+	 * @return <code>true</code> if there must be a zip entry in the stream 
+	 * created before opening the {@link IGISOutputStream}. 
+	 */
+	public boolean requiresZipEntry() {
+		return requiresZipEntry;
+	}
 }
