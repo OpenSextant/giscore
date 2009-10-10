ef8360689657c9e8bfe6941f4a4800dee7928dc6
Jason Mathews <mathews@mitre.org>
2009-10-10 22:45:10 +0000

    added gx:extensions (clampToSeaFloor, relativeToSeaFloor) as enumerations
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@539 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/src/org/mitre/giscore/events/AltitudeModeEnumType.java
+++ giscore/src/org/mitre/giscore/events/AltitudeModeEnumType.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 /**
  * Maps to altitudeModeEnumType in KML.
  * see http://code.google.com/apis/kml/documentation/kmlreference.html#altitudemode
+ *
+ * Also includes altitude types from gx:altitudeMode KML extension schema xmlns:gx=http://www.google.com/kml/ext/2.2
+ * see http://code.google.com/apis/kml/schema/kml22gx.xsd
  * 
  * @author Jason Mathews, MITRE Corp.
  * Date: May 28, 2009 2:07:37 PM
@@ -15,10 +18,20 @@ public enum AltitudeModeEnumType {
 
     clampToGround, // default
     relativeToGround,
-    absolute;
+    absolute,
+	clampToSeaFloor,
+	relativeToSeaFloor;
 
     private static final Logger log = LoggerFactory.getLogger(AltitudeModeEnumType.class);
 
+	/**
+	 * Get normalized altitude Mode such that if altitudeMode value is valid
+	 * then associated AltitudeModeEnumType enumeration is returned otherwise
+	 * null. Null or empty string also returns null. 
+	 * @param altitudeMode ([clampToGround], relativeToGround, absolute)
+	 * 		also including gx:extensions (clampToSeaFloor, relativeToSeaFloor)
+	 * @return Enumerated type instance if valid otherwise null
+	 */
     public static AltitudeModeEnumType getNormalizedMode(String altitudeMode) {
         if (StringUtils.isNotBlank(altitudeMode))
             try {
