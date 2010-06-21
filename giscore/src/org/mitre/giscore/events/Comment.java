185870eea6b16e595d8ff301d5567208e94becb5
Jason Mathews <mathews@mitre.org>
2010-06-21 15:16:24 +0000

    added hashCode() + Equals() to Comment
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@700 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/src/org/mitre/giscore/events/Comment.java
+++ giscore/src/org/mitre/giscore/events/Comment.java
@@ -2,6 +2,8 @@ package org.mitre.giscore.events;
 
 import java.io.Serializable;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.mitre.giscore.IStreamVisitor;
 
 /**
@@ -56,6 +58,26 @@ public class Comment implements IGISObject, Serializable {
 	 */
 	public void accept(IStreamVisitor visitor) {
 		visitor.visit(this);
+	}
+
+    /*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 	/**
