a20796400e6db123097e27f296b231068f1bc247
Jason Mathews <mathews@mitre.org>
2010-11-15 14:12:45 +0000

    remove unused import
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@1048 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/src/org/mitre/giscore/geometry/GeometryBag.java
+++ giscore/src/org/mitre/giscore/geometry/GeometryBag.java
@@ -25,7 +25,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import org.apache.commons.lang.builder.ToStringBuilder;
