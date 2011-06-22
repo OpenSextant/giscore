61479ef21844d4b055a27e7949305c213e1a4a17
Jason Mathews <mathews@mitre.org>
2011-06-22 15:02:12 +0000

    added copyright and updated javadoc
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@1234 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/src/org/mitre/giscore/events/AbstractObject.java
+++ giscore/src/org/mitre/giscore/events/AbstractObject.java
@@ -1,3 +1,17 @@
+/****************************************************************************************
+ *  AbstractObject.java
+ *
+ *  (C) Copyright MITRE Corporation 2009
+ *
+ *  The program is provided "as is" without any warranty express or implied, including
+ *  the warranty of non-infringement and the implied warranties of merchantability and
+ *  fitness for a particular purpose.  The Copyright owner will not be liable for any
+ *  damages suffered by you as a result of using the Program.  In no event will the
+ *  Copyright owner be liable for any special, indirect or consequential damages or
+ *  lost profits even if the Copyright owner has been advised of the possibility of
+ *  their occurrence.
+ *
+ ***************************************************************************************/
 package org.mitre.giscore.events;
 
 import edu.umd.cs.findbugs.annotations.CheckForNull;
@@ -71,7 +85,9 @@ public abstract class AbstractObject implements IDataSerializable, IGISObject {
 	/* (non-Javadoc)
 	 * @see org.mitre.giscore.utils.IDataSerializable#readData(org.mitre.giscore.utils.SimpleObjectInputStream)
 	 */
-	public void readData(SimpleObjectInputStream in) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
+	public void readData(SimpleObjectInputStream in) throws IOException,
+			ClassNotFoundException, InstantiationException,
+			IllegalAccessException {
 		id = in.readString();
 	}
 
