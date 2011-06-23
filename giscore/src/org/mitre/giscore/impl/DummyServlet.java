9ee4447af6ecfbbb81112f247b85ae51723349df
Jason Mathews <mathews@mitre.org>
2011-06-23 20:17:48 +0000

    remove bogus @Override
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@1253 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/src/org/mitre/giscore/impl/DummyServlet.java
+++ giscore/src/org/mitre/giscore/impl/DummyServlet.java
@@ -28,31 +28,26 @@ import javax.servlet.ServletResponse;
 
 public class DummyServlet implements Servlet {
 
-	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public ServletConfig getServletConfig() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public String getServletInfo() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void init(ServletConfig arg0) throws ServletException {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void service(ServletRequest arg0, ServletResponse arg1)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
