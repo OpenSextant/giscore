81d1995f86a01dbc9dfb043750b7aeb65c6d7279
Jason Mathews <mathews@mitre.org>
2012-04-18 20:00:07 +0000

    add more cases
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@1510 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/test/org/mitre/giscore/test/TestStyle.java
+++ giscore/test/org/mitre/giscore/test/TestStyle.java
@@ -16,9 +16,13 @@ public class TestStyle {
 	@Test
 	public void testStyle() {
 		Style style = new Style("123");
-		style.setIconStyle(Color.red, 1.4, "http://maps.google.com/mapfiles/kml/shapes/airports.png");
+		style.setIconStyle(Color.red, null);
+		assertTrue(style.hasIconStyle());
+
+		style.setIconUrl("http://maps.google.com/mapfiles/kml/shapes/airports.png");
 		style.setListStyle(Color.GREEN, Style.ListItemType.check);
 		style.setBalloonStyle(Color.BLUE, "text $[description]", Color.BLACK, "default");
+
 		assertNotNull(style.getId());
 		assertNotNull(style.getIconUrl());
 		assertTrue(style.hasIconStyle());
@@ -33,15 +37,22 @@ public class TestStyle {
 	@Test
 	public void testStyleCopyEquals() {
 		Style s1 = new Style("123");
-		s1.setIconStyle(Color.red, 1.4, "http://maps.google.com/mapfiles/kml/shapes/airports.png");
-		s1.setListStyle(Color.GREEN, Style.ListItemType.check);
 		s1.setBalloonStyle(Color.BLUE, "text $[description]", Color.BLACK, "default");
+		s1.setIconStyle(Color.red, 1.4, "http://maps.google.com/mapfiles/kml/shapes/airports.png");
+		s1.setLabelStyle(Color.BLUE, 1.1);
+		s1.setLineStyle(Color.BLUE, 0.0);
+		s1.setListStyle(Color.GREEN, Style.ListItemType.check);
 
 		Style s2 = new Style(s1);
 		assertEquals(s1, s2);
+		assertEquals(s1.hashCode(), s2.hashCode());
 
 		s2.setListStyle(Color.BLACK, Style.ListItemType.check);
 		assertFalse(s1.equals(s2));
 		assertFalse(s2.equals(s1));
+
+		Style s3 = new Style();
+		Style s4 = new Style(s3);
+		assertEquals(s3, s4);
 	}
 }
