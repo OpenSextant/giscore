8aa477dfe7604770961bcfb32ab34fbbfa3f2aa8
Jason Mathews <mathews@mitre.org>
2012-05-12 17:42:45 +0000

    add test for drawOrder
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@1524 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/test/org/mitre/giscore/test/output/TestGeometryOutput.java
+++ giscore/test/org/mitre/giscore/test/output/TestGeometryOutput.java
@@ -13,56 +13,60 @@ import java.util.ArrayList;
 import java.io.ByteArrayOutputStream;
 import java.io.ByteArrayInputStream;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.*;
 
 /**
  * Test reading/writing core geometry classes.
- * 
+ *
  * @author Jason Mathews, MITRE Corp.
- * Date: Oct 5, 2009 3:12:57 PM
+ *         Date: Oct 5, 2009 3:12:57 PM
  */
 public class TestGeometryOutput extends TestGISBase {
 
-	@Test
-	public void testPointCreation() throws Exception {
-		Point cp = getRandomPoint();
-		List<Point> pts = new ArrayList<Point>();
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		SimpleObjectOutputStream os = new SimpleObjectOutputStream(bos);
-		for (int i=0; i < 5; i++) {
-			Point pt = getRingPoint(cp, i, 5, .3, .4);
-			pts.add(pt);
-			pt.writeData(os);
-		}
-		Point pt2 = new Point();
-		SimpleObjectInputStream is = new SimpleObjectInputStream(new ByteArrayInputStream(bos.toByteArray()));
-		for (Point pt : pts) {
-			pt2.readData(is);
-			assertEquals(pt, pt2);
-		}
-	}
+    @Test
+    public void testPointCreation() throws Exception {
+        Point cp = getRandomPoint();
+        List<Point> pts = new ArrayList<Point>();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        SimpleObjectOutputStream os = new SimpleObjectOutputStream(bos);
+        for (int i = 0; i < 5; i++) {
+            Point pt = getRingPoint(cp, i, 5, .3, .4);
+            pts.add(pt);
+            pt.writeData(os);
+        }
+        Point pt2 = new Point();
+        SimpleObjectInputStream is = new SimpleObjectInputStream(new ByteArrayInputStream(bos.toByteArray()));
+        for (Point pt : pts) {
+            pt2.readData(is);
+            assertEquals(pt, pt2);
+        }
+    }
 
-	@Test
-	public void testRingCreation() throws Exception {
-		Point cp = getRandomPoint();
-		List<Point> pts = new ArrayList<Point>();
-		pts.add(getRingPoint(cp, 4, 5, .3, .4));
-		pts.add(getRingPoint(cp, 3, 5, .3, .4));
-		pts.add(getRingPoint(cp, 2, 5, .3, .4));
-		pts.add(getRingPoint(cp, 1, 5, .3, .4));
-		pts.add(getRingPoint(cp, 0, 5, .3, .4));
-		pts.add(pts.get(0)); // ring should start and end with the same point
-		LinearRing ring = new LinearRing(pts, true);
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		SimpleObjectOutputStream os = new SimpleObjectOutputStream(bos);
-		ring.writeData(os);
-		LinearRing ring2 = new LinearRing();
-		SimpleObjectInputStream is = new SimpleObjectInputStream(new ByteArrayInputStream(bos.toByteArray()));
-		ring2.readData(is);
-		assertEquals(ring, ring2);
-	}
+    @Test
+    public void testRingCreation() throws Exception {
+        Point cp = getRandomPoint();
+        List<Point> pts = new ArrayList<Point>();
+        pts.add(getRingPoint(cp, 4, 5, .3, .4));
+        pts.add(getRingPoint(cp, 3, 5, .3, .4));
+        pts.add(getRingPoint(cp, 2, 5, .3, .4));
+        pts.add(getRingPoint(cp, 1, 5, .3, .4));
+        pts.add(getRingPoint(cp, 0, 5, .3, .4));
+        pts.add(pts.get(0)); // ring should start and end with the same point
+        LinearRing ring = new LinearRing(pts, true);
+        ring.setDrawOrder(2);
+        ring.setTessellate(true);
+        ring.setExtrude(true);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        SimpleObjectOutputStream os = new SimpleObjectOutputStream(bos);
+        ring.writeData(os);
+        LinearRing ring2 = new LinearRing();
+        SimpleObjectInputStream is = new SimpleObjectInputStream(new ByteArrayInputStream(bos.toByteArray()));
+        ring2.readData(is);
+        assertTrue(2 == ring2.getDrawOrder());
+        assertTrue(ring2.getTessellate());
+        assertTrue(ring2.getExtrude());
+        assertEquals(ring, ring2);
+    }
 
     @Test
     public void testModelCreation() throws Exception {
@@ -95,7 +99,7 @@ public class TestGeometryOutput extends TestGISBase {
     }
 
     @Test
-	public void testGeometryBagCreation() throws Exception {
+    public void testGeometryBagCreation() throws Exception {
         // create GeometryBag containing: MultiPoint, MultiLine, MultiLinearRings, MultiPolygons, and GeometryBag geometries 
         List<Feature> features = getMultiGeometries();
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -106,9 +110,9 @@ public class TestGeometryOutput extends TestGISBase {
             if (g != null) bag.add(g);
         }
         bag.writeData(os);
-		assertFalse(bag.isEmpty());
-		assertNotNull(bag.getPoints());
-		assertNotNull(bag.getPart(0));
+        assertFalse(bag.isEmpty());
+        assertNotNull(bag.getPoints());
+        assertNotNull(bag.getPart(0));
 
         SimpleObjectInputStream is = new SimpleObjectInputStream(new ByteArrayInputStream(bos.toByteArray()));
         GeometryBag geo2 = new GeometryBag();
