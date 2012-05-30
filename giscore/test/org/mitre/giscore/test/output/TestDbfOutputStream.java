bd363c1b10d5f9a5e231cd52a61b5fb4f090a175
Jason Mathews <mathews@mitre.org>
2012-05-30 14:45:47 +0000

    added testDbfOutputStreamBoolean()
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@1548 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/test/org/mitre/giscore/test/output/TestDbfOutputStream.java
+++ giscore/test/org/mitre/giscore/test/output/TestDbfOutputStream.java
@@ -26,6 +26,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.RandomUtils;
@@ -118,29 +119,71 @@ public class TestDbfOutputStream {
         os.close();
 
         FileInputStream is = new FileInputStream(temp);
-        DbfInputStream dbfis = new DbfInputStream(is, null);
-        Schema readschema = (Schema) dbfis.read();
-        assertNotNull(readschema);
-        assertEquals(3, readschema.getKeys().size());
-        compare(s1, readschema.get("s1"));
-        compare(s2, readschema.get("s2"));
-        compare(s3, readschema.get("s3"));
-        for (int i = 0; i < 50; i++) {
+        try {
+            DbfInputStream dbfis = new DbfInputStream(is, null);
+            Schema readschema = (Schema) dbfis.read();
+            assertNotNull(readschema);
+            assertEquals(3, readschema.getKeys().size());
+            compare(s1, readschema.get("s1"));
+            compare(s2, readschema.get("s2"));
+            compare(s3, readschema.get("s3"));
+            for (int i = 0; i < 50; i++) {
+                Row readrow = (Row) dbfis.read();
+                Row origrow = data.get(i);
+                compare(s, readschema, origrow, readrow);
+            }
+
             Row readrow = (Row) dbfis.read();
-            Row origrow = data.get(i);
-            compare(s, readschema, origrow, readrow);
+            assertNotNull(readrow);
+            assertEquals(5, StringUtils.length((String) readrow.getData(s1)));
+            assertEquals(10, StringUtils.length((String) readrow.getData(s2)));
+            assertEquals(253, StringUtils.length((String) readrow.getData(s3)));
+
+            readrow = (Row) dbfis.read();
+            assertNotNull(readrow);
+            for (Map.Entry<SimpleField, Object> e : readrow.getEntrySet()) {
+                assertTrue(ObjectUtils.NULL == e.getValue());
+            }
+        } finally {
+            IOUtils.closeQuietly(is);
         }
+    }
 
-        Row readrow = (Row) dbfis.read();
-        assertNotNull(readrow);
-        assertEquals(5, StringUtils.length((String) readrow.getData(s1)));
-        assertEquals(10, StringUtils.length((String) readrow.getData(s2)));
-        assertEquals(253, StringUtils.length((String) readrow.getData(s3)));
+    @Test
+    public void testDbfOutputStreamBoolean() throws Exception {
+        Schema s = new Schema();
+        SimpleField b = new SimpleField("b", Type.BOOL);
+        s.put(b);
+        File temp = File.createTempFile("test-bool", ".dbf");
+        FileOutputStream os = new FileOutputStream(temp);
+        DbfOutputStream dbfos = new DbfOutputStream(os, null);
+        dbfos.write(s);
 
-        readrow = (Row) dbfis.read();
-        assertNotNull(readrow);
-        for (Map.Entry<SimpleField, Object> e : readrow.getEntrySet()) {
-            assertTrue(ObjectUtils.NULL == e.getValue());
+        Object[] objs = {Boolean.TRUE, Boolean.FALSE, "?", "t", "f", "1", "0", null, "abc"};
+
+        List<Row> data = new ArrayList<Row>(objs.length);
+        for (Object o : objs) {
+            Row r = new Row();
+            r.putData(b, o);
+            data.add(r);
+            dbfos.write(r);
+        }
+        dbfos.close();
+        os.close();
+
+        FileInputStream is = new FileInputStream(temp);
+        try {
+            DbfInputStream dbfis = new DbfInputStream(is, null);
+            Schema readschema = (Schema) dbfis.read();
+            assertNotNull(readschema);
+            assertEquals(1, readschema.getKeys().size());
+            compare(b, readschema.get("b"));
+            for (Row origrow : data) {
+                Row readrow = (Row) dbfis.read();
+                compare(s, readschema, origrow, readrow);
+            }
+        } finally {
+            IOUtils.closeQuietly(is);
         }
     }
 
@@ -195,7 +238,7 @@ public class TestDbfOutputStream {
 
         // create values larger than most field lengths
         Row r = new Row();
-        r.putData(b, "0"); // String as boolean
+        r.putData(b, Boolean.FALSE);
         r.putData(f, Float.MAX_VALUE);
         r.putData(db, Double.MAX_VALUE);
         r.putData(li, Long.MIN_VALUE);
@@ -294,19 +337,23 @@ public class TestDbfOutputStream {
         os.close();
 
         FileInputStream is = new FileInputStream(temp);
-        DbfInputStream dbfis = new DbfInputStream(is, null);
-        Schema readschema = (Schema) dbfis.read();
-        assertNotNull(readschema);
-        assertEquals(1, readschema.getKeys().size());
-        compare(date, readschema.get("date"));
-        for (Row origrow : data) {
+        try {
+            DbfInputStream dbfis = new DbfInputStream(is, null);
+            Schema readschema = (Schema) dbfis.read();
+            assertNotNull(readschema);
+            assertEquals(1, readschema.getKeys().size());
+            compare(date, readschema.get("date"));
+            for (Row origrow : data) {
+                Row readrow = (Row) dbfis.read();
+                compare(s, readschema, origrow, readrow);
+            }
             Row readrow = (Row) dbfis.read();
-            compare(s, readschema, origrow, readrow);
+            assertNotNull(readrow);
+            assertTrue(readrow.hasExtendedData());
+            assertNull(readrow.getData(date));
+        } finally {
+            IOUtils.closeQuietly(is);
         }
-        Row readrow = (Row) dbfis.read();
-        assertNotNull(readrow);
-        assertTrue(readrow.hasExtendedData());
-        assertNull(readrow.getData(date));
     }
 
     private void compare(SimpleField orig, SimpleField read) {
@@ -323,14 +370,31 @@ public class TestDbfOutputStream {
         Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
         assertNotNull(readrow);
         assertEquals(origrow.getFields().size(), readrow.getFields().size());
-        for (String key : s.getKeys()) {
-            SimpleField field = s.get(key);
+        for (Map.Entry<String, SimpleField> entry : s.entrySet()) {
+            String key = entry.getKey();
+            SimpleField field = entry.getValue();
             Object v1 = origrow.getData(field);
             SimpleField readfield = readschema.get(key);
             Object v2 = readrow.getData(readfield);
-            if (v2 instanceof Date && v1 instanceof String) {
+            if (field.getType() == Type.BOOL) {
+                if (v1 == null) {
+                    // null value -> as false
+                    assertEquals(Boolean.FALSE, v2);
+                    return;
+                } else if (v1 instanceof String) {
+                    // System.out.println("XXX: boolean: orig="+v1+", actual="+v2);
+                    if ("?".equals(v1) && v2 == null) {
+                        // v1 => ? boolean value interpreted as null
+                        return;
+                    } else if (v2 instanceof Boolean) {
+                        assertEquals(getBoolean(v1), v2);
+                        return;
+                    }
+                }
+            } else if (v1 instanceof String && v2 instanceof Date) {
                 v1 = getDate(v1);
             }
+
             if (v1 instanceof Long) {
                 assertEquals(((Number) v1).longValue(), ((Number) v2).longValue());
             } else if (v1 instanceof Number) {
@@ -362,7 +426,7 @@ public class TestDbfOutputStream {
         return randomString(len);
     }
 
-    private String randomString(int len) {
+    private static String randomString(int len) {
         StringBuilder b = new StringBuilder(len);
         for (int i = 0; i < len; i++) {
             int ch;
@@ -391,6 +455,21 @@ public class TestDbfOutputStream {
                 }
             }
             return null;
+        }
+    }
+
+    private static Boolean getBoolean(Object data) {
+        if (data instanceof Boolean) {
+            return (Boolean) data;
+        } else if (data instanceof String) {
+            String val = (String) data;
+            val = val.toLowerCase();
+            if (val.equals("?"))
+                return null;
+            return val.startsWith("t") || val.startsWith("y")
+                    || "1".equals(val);
+        } else {
+            return Boolean.FALSE;
         }
     }
 
