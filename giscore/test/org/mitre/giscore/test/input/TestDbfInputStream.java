f0b4c4e0ce056b0b382e0c238d641cf947507962
Jason Mathews <mathews@mitre.org>
2012-05-31 22:42:34 +0000

    add test with DBF SHORT type len=2
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@1560 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/test/org/mitre/giscore/test/input/TestDbfInputStream.java
+++ giscore/test/org/mitre/giscore/test/input/TestDbfInputStream.java
@@ -29,6 +29,7 @@ import org.mitre.giscore.events.Schema;
 import org.mitre.giscore.input.dbf.DbfInputStream;
 
 public class TestDbfInputStream {
+
 	@Test public void testDbfInputStream1() throws Exception {
         final File file = new File("data/shape/Iraq.dbf");
         if (!file.exists()) {
@@ -49,7 +50,46 @@ public class TestDbfInputStream {
 			obj = dbfs.read();
 		}
 	}
-	
+
+    @Test public void testDbfInputStreamShort() throws Exception {
+        final File file = new File("data/shape/MBTA.dbf");
+        if (!file.exists()) {
+            System.err.println("file not found: " + file);
+            return;
+        }
+        DbfInputStream dbfs = new DbfInputStream(file, new Object[0]);
+        IGISObject obj = dbfs.read();
+        assertNotNull(obj);
+        assertTrue(obj instanceof Schema);
+        Schema s = (Schema) obj;
+        /*
+        <Schema name='schema_2' id='s_2'>
+          <SimpleField name='OBJECTID' type='LONG'/>
+          <SimpleField name='SOURCE' type='STRING'/>
+          <SimpleField name='LINE' type='STRING'/>
+          <SimpleField name='GRADE' type='SHORT'/>
+          <SimpleField name='SHAPE_LEN' type='DOUBLE'/>
+        </Schema>
+         */
+        assertNotNull(s.getKeys());
+        assertTrue(s.getKeys().size() > 1);
+        obj = dbfs.read();
+        while (obj != null) {
+            /*
+            Row data=
+            OBJECTID (LONG) = '1'
+            SOURCE (STRING) = 'DLG'
+            LINE (STRING) = 'SILVER'
+            GRADE (SHORT) = '3'
+            SHAPE_LEN (DOUBLE) = '4575.18028634'
+             */
+            assertNotNull(obj);
+            assertTrue(obj instanceof Row);
+            obj = dbfs.read();
+        }
+        dbfs.close();
+    }
+
 	@Test public void testDbfInputStream2() throws Exception {
         final File file = new File("data/shape/tl_2008_us_metdiv.dbf");
         if (!file.exists()) {
@@ -69,8 +109,8 @@ public class TestDbfInputStream {
 			assertTrue(obj instanceof Row);
 			obj = dbfs.read();
 		}
-
-	}
+        dbfs.close();
+    }
 	
 	@Test public void testDbfInputStream3() throws Exception {
         final File file = new File("data/shape/AlleghenyCounty_Floodplain2000.dbf");
