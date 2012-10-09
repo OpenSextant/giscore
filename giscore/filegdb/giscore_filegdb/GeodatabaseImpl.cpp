e3532c6751d94a2c6b126055f25f93f2d74361c9
Douglas Rand <drand@mitre.org>
2012-10-09 16:49:46 +0000

    Work in progress
    git-svn-id: svn+ssh://developer.mitre.org/svnroot/giscore/trunk@1589 5c99b798-f2dd-11dd-9a12-8f84353f4b73

--- giscore/filegdb/giscore_filegdb/GeodatabaseImpl.cpp
+++ giscore/filegdb/giscore_filegdb/GeodatabaseImpl.cpp
@@ -1,6 +1,8 @@
 #include <FileGDBAPI.h>
 #include "Stdafx.h"
 #include "org_mitre_giscore_filegdb_Geodatabase.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 using namespace FileGDBAPI;
@@ -16,14 +18,20 @@ extern "C" {
 JNIEXPORT jlong JNICALL Java_org_mitre_giscore_filegdb_Geodatabase_open(JNIEnv * env, jobject self, jstring path) {
 	jboolean isCopy;
 	const char* cpath = env->GetStringUTFChars(path, &isCopy);
+
 	Geodatabase *geodatabase = new Geodatabase();
 	fgdbError hr;
 	size_t len = strlen(cpath);
-	wchar_t* wpath = new wchar_t[len+1];
-	mbtowc(wpath, cpath, len);
-
+	wchar_t *buf = new wchar_t[len*2];
+	len = mbstowcs(buf, cpath, len);
+	wstring wpath(buf, len);
+	delete buf;
+	
 	if ((hr = OpenGeodatabase(wpath, *geodatabase)) != S_OK) {
-		delete geodatabase;
+		if (geodatabase != NULL) {
+			delete geodatabase;
+		}
+		cerr << "Creating database failed, HR code " << hr << "\n";
 		return 0;
 	}
 
@@ -51,6 +59,39 @@ JNIEXPORT void JNICALL Java_org_mitre_giscore_filegdb_Geodatabase_close_1db(JNIE
 	delete geodatabase;
 	if (error) env->ThrowNew(clazz, "Bad geodatabase state or other error on close");
 };
+
+
+/**
+ * Get the geodatabase object from the self passed in.
+ */
+Geodatabase* getGeodatabase(JNIEnv *env, jobject self) {
+	jclass clz = env->FindClass("org/mitre/giscore/filegdb/Geodatabase");
+	jfieldID field = env->GetFieldID(clz, "ptr", "J");
+	jlong ptr = env->GetLongField(self, field);
+	return (Geodatabase*) ptr;
+}
+
+/*
+ * Class:     org_mitre_giscore_filegdb_Geodatabase
+ * Method:    getDatasetTypes
+ * Signature: ()[Ljava/lang/String;
+ */
+JNIEXPORT jobjectArray JNICALL Java_org_mitre_giscore_filegdb_Geodatabase_getDatasetTypes(JNIEnv *env, jobject self) {
+	Geodatabase* db = getGeodatabase(env, self);
+	vector<wstring> datasetTypes;
+	fgdbError hr;
+	if ((hr = db->GetDatasetTypes(datasetTypes)) != S_OK) {
+		jclass clazz = env->FindClass("org/mitre/giscore/filegdb/GdbException");
+		env->ThrowNew(clazz, "Failed to find dataset types");
+		return NULL;
+	}
+	jclass strclz = env->FindClass("java/lang/String");
+	jobjectArray rval = env->NewObjectArray(datasetTypes.size(), strclz, env->NewStringUTF(""));
+
+
+
+	return rval;
+}
 
 #ifdef __cplusplus
 }
---
+++ giscore/src/org/mitre/giscore/filegdb/GdbException.java
@@ -0,0 +1,43 @@
+/****************************************************************************************
+ *  GdbException.java
+ *
+ *  Created: Oct 9, 2012
+ *
+ *  @author DRAND
+ *
+ *  (C) Copyright MITRE Corporation 2012
+ *
+ *  The program is provided "as is" without any warranty express or implied, including
+ *  the warranty of non-infringement and the implied warranties of merchantibility and
+ *  fitness for a particular purpose.  The Copyright owner will not be liable for any
+ *  damages suffered by you as a result of using the Program.  In no event will the
+ *  Copyright owner be liable for any special, indirect or consequential damages or
+ *  lost profits even if the Copyright owner has been advised of the possibility of
+ *  their occurrence.
+ *
+ ***************************************************************************************/
+package org.mitre.giscore.filegdb;
+
+public class GdbException extends Exception {
+
+	public GdbException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public GdbException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GdbException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GdbException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
