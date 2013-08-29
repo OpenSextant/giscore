d13bcc9318bcb2cccfd0ead2e47972f46e474e28
John Gibson <jgibson@mitre.org>
2013-08-29 11:50:20 -0700

    FileGDB native code is now loaded from java.library.path if possible.

--- src/main/java/org/opensextant/giscore/input/gdb/FileGDBLibraryLoader.java
+++ src/main/java/org/opensextant/giscore/input/gdb/FileGDBLibraryLoader.java
@@ -26,9 +26,9 @@ import org.apache.commons.lang.StringUtils;
 import org.opensextant.giscore.utils.LibraryLoader;
 
 public class FileGDBLibraryLoader extends LibraryLoader {
-	
+
 	public FileGDBLibraryLoader() throws IOException {
-		super(Package.getPackage("org.opensextant.giscore.filegdb"), "filegdb");
+		super(Package.getPackage("org.opensextant.giscore.filegdb"), "filegdb", true);
 	}
 
 	/* This method causes the debug library to be run while debugging. */
